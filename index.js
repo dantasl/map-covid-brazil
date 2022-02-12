416d8ae797184846b7507914e65de8f57e171c13
Lucas Dantas <lucasdantas.dev@gmail.com>
2022-02-12 14:40:00 -0300

    Toggles view by cases / casualities and displays information for it.

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 google.charts.load('current', {'packages':['geochart']});
 
-fetch('https://api.brasil.io/v1/dataset/covid19/caso/data/?place_type=state', {
-    method: "GET",
-    headers: {"Authorization": "Token 8e91da2bd4acc0021df573785ea3f7893c84c7d1"}
-})
-.then(response => response.json())
-.then(response => response['results'].filter(result => result['is_last']))
-.then(response => drawRegionsMap(response));
+function getData(type) {
+    fetch('https://api.brasil.io/v1/dataset/covid19/caso/data/?place_type=state', {
+        method: "GET",
+        headers: {"Authorization": "Token 8e91da2bd4acc0021df573785ea3f7893c84c7d1"}
+    })
+    .then(response => response.json())
+    .then(response => response['results'].filter(result => result['is_last']))
+    .then(response => drawRegionsMap(response, type));
+}
 
-function drawRegionsMap(stateCases) {
-    var statesArray = [ ['State', 'Confirmed Cases'] ];
+function drawRegionsMap(stateCases, type) {
+    if (!type) return;
+
+    var message = type === 'confirmed' ? 'Confirmed Cases' : 'Confirmed Casualities';
+
+    var statesArray = [ ['State', message] ];
 
     stateCases.forEach(function (item) {
-        statesArray.push(['BR-' + item['state'], item['confirmed']])
+        statesArray.push(['BR-' + item['state'], item[type]]);
     });
 
     var data = google.visualization.arrayToDataTable(statesArray);
@@ -29,4 +35,14 @@ function drawRegionsMap(stateCases) {
 
     var chart = new google.visualization.GeoChart(document.getElementById('regions'));
     chart.draw(data, options);
+}
+
+function showConfirmed() {
+    getData('confirmed');
+    document.getElementById('viewing').innerText = 'You are currently viewing the number of Confirmed Cases.';
+}
+
+function showCasualities() {
+    getData('deaths');
+    document.getElementById('viewing').innerText = 'You are currently viewing the number of Casualities.';
 }
